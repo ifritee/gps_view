156e00ee78c584a7cb5f030a470c69fb910c0d14
ifritee <ifritee@gmail.com>
2022-07-29 22:57:26 +0300

    Добавил коментарии

--- src/gui/DSettings.h
+++ src/gui/DSettings.h
@@ -8,28 +8,44 @@ namespace Ui {
   class DSettings;
 }
 
+/**
+ * @class DSettings
+ * @brief GUI settings dialog. Includes rs232 options.
+ * @author Nikishin E. V.
+ * @date 29.07.2022 */
 class DSettings : public QDialog
 {
   Q_OBJECT
 
 public:
+  /** @brief Constructor */
   explicit DSettings(QWidget *parent = nullptr);
-  ~DSettings();
+  /** @brief Destructor */
+  virtual ~DSettings();
+  /** @brief Return port name with refresh ports */
   QString portName();
+  /** @brief Return bound rate */
   int boundRate();
+  /** @brief Return data bits */
   QSerialPort::DataBits dataBits();
+  /** @brief Return parity */
   QSerialPort::Parity parity();
+  /** @brief Return stop bit */
   QSerialPort::StopBits stopBits();
+  /** @brief Return flow control mode */
   QSerialPort::FlowControl flowControl();
 
 private:
-  Ui::DSettings *ui;
+  Ui::DSettings *ui; ///< @brief GUI view
 
 public slots:
+  /** @brief Seve settings in file */
   void Save_slt();
+  /** @brief Load settings from file */
   void Load_slt();
 
 private slots:
+  /** @brief Refresh ports list */
   void RefreshPorts_slt();
 };
 
--- src/gui/MainWindow.h
+++ src/gui/MainWindow.h
@@ -10,24 +10,34 @@ class QTimer;
 class QSerialPort;
 class DSettings;
 
+/**
+ * @class MainWindow
+ * @brief GUI Main window
+ * @author Nikishin E.V.
+ * @date 29.07.2022 */
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
 
 public:
+  /** @brief Constructor */
   explicit MainWindow(QWidget *parent = nullptr);
+  /** @brief Destructor */
   virtual ~MainWindow();
 
 private:
-  Ui::MainWindow *ui;
-  QTimer * _Timer_po; ///< @brief Таймер считывания данных
-  QSerialPort * _SerialPort_po; ///< @brief RS232 порт
-  QByteArray _ReciveData_o; ///< @brief Принятые данные
-  DSettings * _Settings_po; ///< @brief Диалог настроек
+  Ui::MainWindow *ui; ///< @brief GUI view
+  QTimer * _Timer_po; ///< @brief Timer read data
+  QSerialPort * _SerialPort_po; ///< @brief RS232 port
+  QByteArray _ReciveData_o; ///< @brief Recived data
+  DSettings * _Settings_po; ///< @brief Settings dialog
 
 private slots:
+  /** @brief Start GPS control */
   void StartGPS_slt();
+  /** @brief Stop GPS control */
   void StopGPS_slt();
+  /** @brief Read NMEA string */
   void ReadNMEA_slt();
 };
 
