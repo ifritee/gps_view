ae7455f800f70556cae0dac04200b04a28398063
ifritee <ifritee@gmail.com>
2022-08-01 23:02:54 +0300

    Поправил цвета

--- src/gui/USatelliteItem.cpp
+++ src/gui/USatelliteItem.cpp
@@ -28,11 +28,11 @@ QRectF USatelliteItem::boundingRect() const
 
 void USatelliteItem::paint(QPainter *painter, const QStyleOptionGraphicsItem * /*option*/, QWidget * /*widget*/)
 {
-  painter->setPen(QPen(QBrush(_GSVData_o._SNR_u == 0 ? Qt::white : Qt::transparent), 1));
+  painter->setPen(QPen(QBrush(_GSVData_o._SNR_u == 0 ? Qt::darkGray : Qt::transparent), 1));
   painter->setBrush((_GSVData_o._SNR_u == 0 ? QColor(Qt::transparent) : CConstants::colorBySNR(_GSVData_o._SNR_u)));
   if (_SatelliteType_en == ESATELLITETYPE::GP) { // Если GPS
     painter->drawEllipse(QPointF(0,0), 4, 4);
   }
-  painter->setPen(Qt::white);
+  painter->setPen(_GSVData_o._SNR_u == 0 ? Qt::darkGray : Qt::white);
   painter->drawText(QPointF(-6, 14), QString::number(_GSVData_o._IDSatellite_u).rightJustified(2, '0'));
 }
